16a64c79c7f3bccf922d2685b95357e141e39c08
Kurt Friars <kfriars@apple.com>
2024-03-17 17:19:44 -0400

    Update querying interface

--- src/Contracts/PublisherQueries.php
+++ src/Contracts/PublisherQueries.php
@@ -15,4 +15,19 @@ interface PublisherQueries
      * Scope the query to models that are not in the published state
      */
     public function onlyDraft(): Builder&PublisherQueries;
+
+    /**
+     * Scope the query to models that are not queued for deletion
+     */
+    public function withoutQueuedDeletes(): Builder&PublisherQueries;
+
+    /**
+     * Scope the query to models that are queued for deletion
+     */
+    public function onlyQueuedDeletes(): Builder&PublisherQueries;
+
+    /**
+     * Scope the query to models that are either queued for deletion or not
+     */
+    public function withQueuedDeletes(): Builder&PublisherQueries;
 }
